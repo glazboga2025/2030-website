25a1aad4693dcf247cac5ffe75d8773a54576de6
Gost <gost@MacBook-Pro-Maksim.local>
2025-03-14 13:57:45 +0700

    Исправление конфигурации для GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -14,32 +14,6 @@ const config: NextConfig = {
       },
     ],
   },
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          // For security:
-          {
-            key: "X-Frame-Options",
-            value: "DENY",
-          },
-          {
-            key: "X-Content-Type-Options",
-            value: "nosniff",
-          },
-          {
-            key: "Referrer-Policy",
-            value: "origin-when-cross-origin",
-          },
-          {
-            key: "X-XSS-Protection",
-            value: "1; mode=block",
-          },
-        ],
-      },
-    ];
-  },
 };
 
 export default config;
